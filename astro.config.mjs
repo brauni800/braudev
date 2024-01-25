dce421f703b7fe08e12dfe019e861f670a842a62
Braulio Betancourt <braulio.1796@gmail.com>
2024-01-25 09:44:03 -0600

    build: :construction_worker: add date-fns and astro-compressor libraries

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config'
-
+import compressor from 'astro-compressor'
 import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
 import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), compress()],
+  integrations: [react(), tailwind(), compressor()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
