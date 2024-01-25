5005e57cf20a673453191e76aebb57fc23f704e5
Braulio Betancourt <braulio.1796@gmail.com>
2024-01-25 09:43:02 -0600

    chore: :globe_with_meridians: new translations for hero section

--- src/i18n/translations/en.ts
+++ src/i18n/translations/en.ts
@@ -3,5 +3,11 @@ export const en = {
   'theme.mode.light': 'Light',
   'theme.mode.dark': 'Dark',
   'theme.button.label': 'Change theme',
-  'locale.button.label': 'Change language'
+  'locale.button.label': 'Change language',
+  'section.hero.welcome': 'Hi 👋 I am',
+  'section.hero.presentation.1': 'Front-End Developer since ',
+  'section.hero.presentation.1.5': 'years ago',
+  'section.hero.presentation.2': 'Development of functional and simple sites, prioritizing user experience and efficiency.',
+  'section.hero.about-me.1': 'Web developer with focus on React and Node.',
+  'section.hero.about-me.2': 'I create simple and effective digital solutions to improve the user experience.'
 }
--- src/i18n/translations/es.ts
+++ src/i18n/translations/es.ts
@@ -3,6 +3,11 @@ export const es = {
   'theme.mode.light': 'Claro',
   'theme.mode.dark': 'Obscuro',
   'theme.button.label': 'Cambiar tema',
-  'locale.button.label': 'Cambiar idioma'
-
+  'locale.button.label': 'Cambiar idioma',
+  'section.hero.welcome': 'Hola 👋 soy',
+  'section.hero.presentation.1': 'Desarrollador Front-End desde hace ',
+  'section.hero.presentation.1.5': 'años',
+  'section.hero.presentation.2': 'Desarrollo sitios funcionales y simples, priorizando la experiencia del usuario y la eficiencia.',
+  'section.hero.about-me.1': 'Desarrollador web con enfoque en React y Node.',
+  'section.hero.about-me.2': 'Creo soluciones digitales simples y efectivas para mejorar la experiencia del usuario.'
 }
