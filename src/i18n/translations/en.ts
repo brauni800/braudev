5f6aa8615708b9e7b251f9f7c1d0f7a4abf9887f
Braulio Betancourt <braulio.1796@gmail.com>
2024-01-26 12:48:34 -0600

    fix: :adhesive_bandage: fix hero position

--- src/i18n/translations/en.ts
+++ src/i18n/translations/en.ts
@@ -6,7 +6,7 @@ export const en = {
   'locale.button.label': 'Change language',
   'section.hero.welcome': 'Hi 👋 I am',
   'section.hero.presentation.1': 'Front-End Developer since ',
-  'section.hero.presentation.1.5': 'years ago',
+  'section.hero.presentation.1.5': 'years ago.',
   'section.hero.presentation.2': 'Development of functional and simple sites, prioritizing user experience and efficiency.',
   'section.hero.about-me.1': 'Web developer with focus on React and Node.',
   'section.hero.about-me.2': 'I create simple and effective digital solutions to improve the user experience.'
